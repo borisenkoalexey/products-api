da04b36ba304b1de3fb0ba5e545cf15b75fb5dbf
borisenkoalexey <borisenkoalexey86@gmail.com>
2026-07-07 19:35:06 +0300

    Добавлен healthcheck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from products-api!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/health")
+def healthcheck():
+    return {"status": "ok"}
